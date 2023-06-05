e8c23b4187285c7e5c7166ab9de0c134d61e80f4
root <root@ip-172-31-13-1.ap-south-1.compute.internal>
2023-06-05 18:37:00 +0000

    Committed first time for the push

---
+++ src/main/java/com/cloudbots/www/App.java
@@ -0,0 +1,13 @@
+package com.cloudbots.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Cloud_Bots" );
+    }
+}
